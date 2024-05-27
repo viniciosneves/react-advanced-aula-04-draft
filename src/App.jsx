5a2885ac86f5ed18ef959730553519c40a891dec
Vinicios Neves <vinicios.neves@mac>
2024-05-27 11:29:32 +0100

    aula 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Heading from './components/Heading'
 import Fieldset from './components/Fieldset'
 import Label from './components/Label'
 import Input from './components/Input'
-import ErrorText from './components/ErrorText'
 import Button from './components/Button'
 import Form from './components/Form'
+import { Formik } from 'formik'
 
 function App() {
+
+  const createUser = (values) => {
+    console.log(values)
+  }
 
   return (
     <>
@@ -17,38 +21,45 @@ function App() {
         <Heading>
           Cadastro
         </Heading>
-        <Form>
-          <Fieldset>
-            <Label>
-              Nome
-            </Label>
-            <Input />
-            <ErrorText>
-              Nome é obrigatório
-            </ErrorText>
-          </Fieldset>
-          <Fieldset>
-            <Label>
-              E-mail
-            </Label>
-            <Input />
-          </Fieldset>
-          <Fieldset>
-            <Label>
-              Senha
-            </Label>
-            <Input />
-          </Fieldset>
-          <Fieldset>
-            <Label>
-              Confirme sua senha
-            </Label>
-            <Input />
-          </Fieldset>
-          <Button>
-            Enviar
-          </Button>
-        </Form>
+        <Formik
+          onSubmit={createUser}
+          initialValues={{
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+          }}
+        >
+          <Form>
+            <Fieldset>
+              <Label>
+                Nome
+              </Label>
+              <Input name="name"/>
+            </Fieldset>
+            <Fieldset>
+              <Label>
+                E-mail
+              </Label>
+              <Input name="email" type="email" />
+            </Fieldset>
+            <Fieldset>
+              <Label>
+                Senha
+              </Label>
+              <Input name="password" type="password"/>
+            </Fieldset>
+            <Fieldset>
+              <Label>
+                Confirme sua senha
+              </Label>
+              <Input name="confirmPassword" type="password"/>
+            </Fieldset>
+            <Button type='submit'>
+              Enviar
+            </Button>
+          </Form>
+        </Formik>
       </Container>
     </>
   )
