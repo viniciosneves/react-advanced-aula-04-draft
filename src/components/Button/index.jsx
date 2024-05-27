54a816180b7a1cd3624410f39fba7ae645fa740b
Vinicios Neves <vinicios.neves@mac>
2024-05-27 10:24:04 +0100

    projeto react

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    width: 100%;
+    background-color: #775DA6;
+    border: none;
+    border-radius: 8px;
+    padding: 12px;
+    color: white;
+    font-size: 18px;
+`
+    
+export default Button
---
+++ src/components/Container/index.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+    width: 848px;
+    max-width: 85%;
+    background-color: white;
+    border-radius: 12px;
+    margin: 24px auto;
+    padding: 24px;
+`
+
+export default Container
